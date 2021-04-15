70fb1136f29a0236c9c694b7b9103c4b6bf3adfe
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-04-14 17:49:21 -0700

    add fields to users devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_000214) do
+ActiveRecord::Schema.define(version: 2021_04_15_004412) do
 
-  create_table "models", force: :cascade do |t|
+  create_table "tweets", force: :cascade do |t|
+    t.text "tweet"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -20,14 +26,10 @@ ActiveRecord::Schema.define(version: 2021_04_15_000214) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
-  create_table "tweets", force: :cascade do |t|
-    t.text "tweet"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
